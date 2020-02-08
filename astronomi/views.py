1d6dd50847f15e5083e25390356e80a40f505d74
hvarmis21 <h.varmis21@gmail.com>
2020-02-08 18:56:14 +0300

    search endpoint eklendi

--- astronomi/views.py
+++ astronomi/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render,HttpResponse,redirect
 from .forms import RegisterForm,LoginForm
 from django.contrib.auth import authenticate,login, logout
 import requests
+import json
 # Create your views here.
 def home_view(request):
-
+    if request.method == "POST":
+        if request.POST["search_list"]:
+            return redirect("/search/{}".format(request.POST["search_list"]))
     if request.user.is_authenticated:
         response = requests.get('https://api.nasa.gov/planetary/apod?api_key=pdA7MvW4yL8zwmgyKHz3BnM8WRKR715bFowEYiU8')
         return render(request, "anasayfa.html", {"response": response.json()})
@@ -35,4 +38,13 @@ def register_view(request):
 
 def logout_view(request):
     logout(request)
-    return render(request, "anasayfa.html")
+    return redirect("home")
+
+def search_view(request,search_word):
+    response = requests.get('https://images-api.nasa.gov/search?q={}'.format(search_word))
+    items = []
+    for item in response.json()['collection']['items'][:5]:
+        items.append({'link': item['links'][0]['href'], 'data': item['data'][0]['photographer'],
+                      'description': item['data'][0]['description']})
+
+    return render(request, "search.html", {'response': items})
--- houseofapps/urls.py
+++ houseofapps/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('login', login_view, name="login"),
     path('logout', logout_view, name="logout"),
     path('register', register_view, name="register"),
+    path('search/<search_word>', search_view, name="search"),
 ]
